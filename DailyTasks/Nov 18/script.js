90a2e1b93cfe69d5ab536c5c714a54c06193383f
Mohanraaji <mohanrajib21@gmail.com>
2024-11-19 16:07:31 +0530

    commit

---
+++ DailyTasks/Nov 18/script.js
@@ -0,0 +1,26 @@
+document.getElementById("calculate").addEventListener("click", () => {
+    const num1 = Number(document.getElementById("num1").value);
+    const num2 = Number(document.getElementById("num2").value);
+    const operator = document.getElementById("operator").value;
+    let result;
+  
+    switch (operator) {
+      case "+":
+        result = num1 + num2;
+        break;
+      case "-":
+        result = num1 - num2;
+        break;
+      case "*":
+        result = num1 * num2;
+        break;
+      case "/":
+        result = num2 !== 0 ? num1 / num2 : "Cannot divide by zero";
+        break;
+      default:
+        result = "Invalid operator";
+    }
+  
+    document.getElementById("result").textContent = `Result: ${result}`;
+  });
+  
